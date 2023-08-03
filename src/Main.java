e1e10306cdb5d92bcaf8a6df07dc279d5f3980b5
tituskinu <mwaura.tk@gmail.com>
2023-08-02 20:10:53 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Titus!");
+        System.out.println("i love my self");
+        System.out.println("net time i will defend my self");
     }
 }
